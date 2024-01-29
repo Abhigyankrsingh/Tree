8bb16dfcfeeea153fdcd9bdfe6b9aca436b552e7
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-29 23:31:56 +0530

    code_updated

---
+++ level.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    class Solution {
+  
+  public:
+     vector<vector<int>> LevelOrder(TreeNode* node){
+
+        vector<vector>>ans;
+
+        if(root == NULL) return ans;
+
+        queue<TreeNode*>q;
+
+     }
+
+    };
+}
